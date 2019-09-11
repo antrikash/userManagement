0e2a3a98d6f1025cd3173adf4f2db6c8275081b6
antrikash <antriksh.akshu@gmail.com>
2019-09-11 19:38:19 +0530

    newchanges
    delete and edit feature added

---
+++ src/Pagination.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+class Pagination extends React.Component {
+  handlePrev = () => {
+    this.props.pageChange(this.props.page - 1);
+  };
+  handleNext = () => {
+    this.props.pageChange(this.props.page + 1);
+  };
+
+  mapFunc = () => {
+    const totalPage = this.props.totalPages;
+    const arr = [];
+
+    for (let i = 1; i <= totalPage; i++) {
+      arr.push(
+        <li class="page-item" key={i}>
+          <span
+            class="page-link"
+            href="#"
+            onClick={() => this.props.pageChange(i)}
+          >
+            {i}
+          </span>
+        </li>
+      );
+    }
+    console.log(arr);
+    return arr;
+  };
+  render() {
+    return (
+      <div>
+        <nav aria-label="Page navigation example">
+          <ul class="pagination">
+            <li class="page-item">
+              <span onClick={this.handlePrev} className="page-link">
+                Previous
+              </span>
+            </li>
+            {this.mapFunc()}
+
+            <li class="page-item">
+              <span onClick={this.handleNext} className="page-link">
+                Next
+              </span>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+export default Pagination;
--- src/index.js
+++ src/index.js
@@ -2,55 +2,100 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
-
+import Pagination from "./Pagination";
 class App extends React.Component {
   state = {
-    data: []
+    data: [],
+    page: 1,
+    totalPages: null
   };
 
-  componentDidMount() {
-    const URL = "https://reqres.in/api/users";
+  fetch2() {
+    const URL = `https://reqres.in/api/users?page=${this.state.page}`;
     fetch(URL)
       .then(function(response) {
         return response.json();
       })
-      .then(
-        x => this.setState({ data: x.data })
-        // this.setState({
-        //   data
-        // })
-      );
+      .then(x => {
+        this.setState({
+          data: x.data,
+          page: x.page,
+          totalPages: x.total_pages
+        });
+      });
   }
+
+  componentDidMount() {
+    this.fetch2();
+  }
+
+  removeData = id => {
+    const data = this.state.data;
+    const newData = data.filter(e => e.id !== id);
+    console.log("XYZ", newData);
+    this.setState({
+      data: newData
+    });
+  };
+  // words.filter(word => word.length > 6);
   func = () => {
     var d = this.state.data;
     return d.map((el, i) => {
       return (
         <tr key={i}>
-          <th scope="row">{i + 1}</th>
+          <th scope="row">{el.id}</th>
           <td>
-            <img alt="profile" src={el.avatar} />
+            <img alt="profile" className="icon" src={el.avatar} />
           </td>
           <td>{el.first_name}</td>
           <td>{el.last_name}</td>
+          <td>
+            <button class="btn btn-secondary">Edit</button>
+            <span> </span>
+            <button
+              onClick={() => this.removeData(el.id)}
+              class="btn btn-danger"
+            >
+              Delete
+            </button>
+          </td>
         </tr>
       );
     });
   };
 
+  pageChange = page => {
+    this.setState(
+      {
+        page
+      },
+      () => this.fetch2()
+    );
+  };
   render() {
     return (
-      <div>
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">First Name</th>
-              <th scope="col">First Name</th>
-              <th scope="col">Last Name</th>
-            </tr>
-          </thead>
-          <tbody>{this.state.data.length > 0 && this.func()}</tbody>
-        </table>
+      <div class="container">
+        <div class="panel-body">
+          <div class="table-responsive">
+            <table class="table table-striped table-dark">
+              <thead>
+                <tr>
+                  <th scope="col">ID</th>
+                  <th scope="col">AVATAR</th>
+                  <th scope="col">First Name</th>
+                  <th scope="col">Last Name</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>{this.state.data.length > 0 && this.func()}</tbody>
+            </table>
+            <Pagination
+              page={this.state.page}
+              totalPages={this.state.totalPages}
+              pageChange={this.pageChange}
+            />
+          </div>
+        </div>
       </div>
     );
   }
